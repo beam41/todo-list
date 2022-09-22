9c85fd0e0da0fa2a3ac32a00d746d7032e8fb4e4
Poom <poch@manaosoftware.com>
2022-09-22 16:00:36 +0700

    use add-dispatch and delete-dispatch on drag end

--- src/Components/Workspace.tsx
+++ src/Components/Workspace.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { useStore } from "react-redux";
+import { Status } from "../Models/Backlog.Model";
 import "../Styles/Workspace.css";
+import { getCardByStatus, updateCardStatus } from "../utils/cardService";
+import { addCard, deleteCard } from "../utils/dispatchAction";
 import Lane from "./Lane";
 
 export default function Workspace() {
-  const titles = ["TODO", "INPROCESS", "DONE"];
+  const store = useStore();
+  const titles = ["TODO", "DOING", "DONE"];
   const onDragEnd = (result: DropResult) => {
-    console.log(result);
+    const { destination, source, draggableId } = result;
+
+    // !destination for undroppable area
+    // destination.droppableId === source.droppableId for same area
+    if (!destination || destination.droppableId === source.droppableId) {
+      return;
+    }
+
+    const originId = source.droppableId;
+    const desId = destination.droppableId as Status;
+    const data = getCardByStatus(draggableId, source.droppableId, store);
+    const newData = updateCardStatus(data, desId)
+    store.dispatch(deleteCard(originId, data.id));
+    store.dispatch(addCard(desId, newData));
   };
+
   return (
     <div className="ws-container">
       <DragDropContext onDragEnd={onDragEnd}>
         {titles.map((status) => {
-          return (
-            <Lane key={status} id={status} title={status}/>
-          );
+          return <Lane key={status} id={status} title={status} />;
         })}
       </DragDropContext>
     </div>
